170a0a870b8b7ca76a7cc0ea96462b32d602a658
Jeremy <veniveci@aliyun.com>
2018-04-16 22:27:48 +0800

    first commit

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -8,18 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// A delay function
+func delay(_ seconds: Double, completion: @escaping ()->Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + seconds, execute: completion)
+}
 
+class ViewController: UIViewController {
+    
+    // MARK: IB outlets
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var heading: UILabel!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var cloud1: UIImageView!
+    @IBOutlet weak var cloud2: UIImageView!
+    @IBOutlet weak var cloud3: UIImageView!
+    @IBOutlet weak var cloud4: UIImageView!
+    
+    // MARK: further UI
+    
+    let spinner = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+    let status = UIImageView(image: UIImage(named: "banner"))
+    let label = UILabel()
+    let messages = ["Connecting ...", "Authorizing ...", "Sending credentials ...", "Failed"]
+    
+    var statusPosition = CGPoint.zero
+    
+    // MARK: view controller methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //set up the UI
+        loginButton.layer.cornerRadius = 8.0
+        loginButton.layer.masksToBounds = true
+        
+        spinner.frame = CGRect(x: -20.0, y: 6.0, width: 20.0, height: 20.0)
+        spinner.startAnimating()
+        spinner.alpha = 0.0
+        loginButton.addSubview(spinner)
+        
+        status.isHidden = true
+        status.center = loginButton.center
+        view.addSubview(status)
+        
+        label.frame = CGRect(x: 0.0, y: 0.0, width: status.frame.size.width, height: status.frame.size.height)
+        label.font = UIFont(name: "HelveticaNeue", size: 18.0)
+        label.textColor = UIColor(red: 0.89, green: 0.38, blue: 0.0, alpha: 1.0)
+        label.textAlignment = .center
+        status.addSubview(label)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        heading.center.x -= view.bounds.width;
+        username.center.x -= view.bounds.width;
+        password.center.x -= view.bounds.width;
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    // MARK: further methods
+    
+    @IBAction func login() {
+        view.endEditing(true)
+    }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let nextField = (textField === username) ? password : username
+        nextField?.becomeFirstResponder()
+        return true
+    }
+    
 }
 
