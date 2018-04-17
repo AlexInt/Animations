174c9db34d9bc5a15cd1b024c5b952b68d1a9edc
Jeremy <veniveci@aliyun.com>
2018-04-18 00:45:28 +0800

    Getting Started with View Animations -- sec01-chapter01

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -63,14 +63,54 @@ class ViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        //将label和textfield移到左边一个屏幕宽度
         heading.center.x -= view.bounds.width;
         username.center.x -= view.bounds.width;
         password.center.x -= view.bounds.width;
+        
+        cloud1.alpha = 0.0;
+        cloud2.alpha = 0.0;
+        cloud3.alpha = 0.0;
+        cloud4.alpha = 0.0;
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        //添加一个移动复位的动画
+        UIView.animate(withDuration: 0.5) {
+            self.heading.center.x += self.view.bounds.width;
+        };
+        //[] means no special options
+        //[] 空数组意味着没有需要指定的动画定制选项。
+        UIView.animate(withDuration: 0.5, delay: 0.3, options: [], animations: {
+            self.username.center.x += self.view.bounds.width;
+        }, completion: nil);
+        /*
+         UIViewAnimationOptions
+         .repeat : 重复执行animations闭包中的代码
+         .autoreverse : 对动画的轨迹进行反向运动
+         两个一起可以构成一种来回不断移动的动画效果
+         options参数的数组中有一个元素是可以直接写一个元素，而不必写[]包围这个元素
+         */
+        //.repeat, .autoreverse, .curveEaseIn
+        UIView.animate(withDuration: 0.5, delay: 0.4, options: [], animations: {
+            self.password.center.x += self.view.bounds.width;
+        }, completion: nil);
+        
+        UIView.animate(withDuration: 0.5, delay: 0.5, options: [], animations: {
+            self.cloud1.alpha = 1;
+        }, completion: nil);
+        UIView.animate(withDuration: 0.5, delay: 0.7, options: [], animations: {
+            self.cloud2.alpha = 1;
+        }, completion: nil);
+        UIView.animate(withDuration: 0.5, delay: 0.9, options: [], animations: {
+            self.cloud3.alpha = 1;
+        }, completion: nil);
+        UIView.animate(withDuration: 0.5, delay: 1.1, options: [], animations: {
+            self.cloud4.alpha = 1;
+        }, completion: nil);
     }
     
     // MARK: further methods
