f5343ceddf05d587db1a58069a248b43fe9d7158
Jeremy <veniveci@aliyun.com>
2018-04-18 23:09:52 +0800

    Spings animation end

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -69,11 +69,15 @@ class ViewController: UIViewController {
         username.center.x -= view.bounds.width;
         password.center.x -= view.bounds.width;
         
+        //对云形的图片---设置动画初始值
         cloud1.alpha = 0.0;
         cloud2.alpha = 0.0;
         cloud3.alpha = 0.0;
         cloud4.alpha = 0.0;
         
+        //对button的处理--设置动画初始值
+        loginButton.center.y += 30.0;
+        loginButton.alpha = 0.0;
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -111,12 +115,31 @@ class ViewController: UIViewController {
         UIView.animate(withDuration: 0.5, delay: 1.1, options: [], animations: {
             self.cloud4.alpha = 1;
         }, completion: nil);
+        
+        /*
+         usingSpringWithDamping: 弹簧的硬度或粘度(可以立即为阻力) 0.0 - 1.0
+         initialSpringVelocity:  弹簧的初始速度 0.0 - 1，0
+         */
+        UIView.animate(withDuration: 3.0, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0, options: [], animations: {
+            self.loginButton.center.y -= 30.0;
+            self.loginButton.alpha = 1.0;
+        }, completion: nil);
     }
     
     // MARK: further methods
     
     @IBAction func login() {
         view.endEditing(true)
+        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
+            self.loginButton.bounds.size.width += 90.0;
+        }, completion: nil);
+        
+        UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: [], animations: {
+            self.loginButton.center.y += 60.0;
+            self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0);
+            self.spinner.center = CGPoint(x: 40.0, y: self.loginButton.frame.size.height/2);
+            self.spinner.alpha = 1.0;
+        }, completion: nil);
     }
     
     // MARK: UITextFieldDelegate
